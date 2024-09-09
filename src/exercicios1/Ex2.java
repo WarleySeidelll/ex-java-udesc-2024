29d60db6fed68a8d81581477704e352df3e3556d
Rafael Zapalai <10880893990@edu.udesc.br>
2024-09-09 19:21:45 -0300

    Ex2

---
+++ src/exercicios1/Ex2.java
@@ -0,0 +1,18 @@
+package exercicios1;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("digite uma distancia percorrida:");
+        double distancia;
+        distancia = sc.nextDouble();
+        System.out.println("informe o combustivel gasto em litros:");
+        double media;
+        double combustivel;
+        combustivel = sc.nextDouble();
+        media = (distancia/combustivel);
+        System.out.printf("a media do veiculo é %.2f por litro",media);
+    }
+}
