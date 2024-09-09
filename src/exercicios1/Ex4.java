316abcfbac2b59b9aed454e5c79d00d66e7a2e49
Rafael Zapalai <10880893990@edu.udesc.br>
2024-09-09 20:43:35 -0300

    Ex4

---
+++ src/exercicios1/Ex4.java
@@ -0,0 +1,17 @@
+package exercicios1;
+import java.util.Scanner;
+public class Ex4 {
+    public static void main (String[] args){
+        String sexo;
+        Scanner sx = new Scanner(System.in);
+        System.out.println("digite o seu sexo (M(masculino) ou F(feminino)");
+        sexo = sx.nextLine();
+        if(sexo.equals("M")){
+            System.out.println("Masculino");
+        }else if(sexo.equals("F")){
+            System.out.println("Feminino");
+        }else{
+            System.out.println("nao valido!");
+        }
+    }
+}
